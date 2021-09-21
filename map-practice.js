5ab21ed9c27c6dd5f48c9a5c8621749f0ebc1805
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-21 16:22:53 -0500

    Changed solution numbers and added solutions 2 and 5

--- map-practice.js
+++ map-practice.js
@@ -6,6 +6,11 @@ var numbersTimes3 = numbers.map((number) => number * 3);
 console.log(numbersTimes3);
 
 // 2
+var strings = ["hello", "goodbye"];
+var upperStrings = strings.map((word) => word.toUpperCase());
+console.log(upperStrings);
+
+// 3
 var people = [
   { name: "Alice", age: 27 },
   { name: "Blane", age: 16 },
@@ -13,6 +18,10 @@ var people = [
 var peopleNames = people.map((person) => person.name);
 console.log(peopleNames);
 
-// 3
+// 4
 var plus7 = numbers.map((number) => number + 7);
 console.log(plus7);
+
+// 5
+var stringLength = strings.map((word) => word.length);
+console.log(stringLength);
