46ac1a88e54a9b0e3493b30855154d6b066142bf
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-21 16:29:30 -0500

    Added solution 2

--- select-practice.js
+++ select-practice.js
@@ -4,3 +4,8 @@
 var numbers = [2, 32, 80, 18, 12, 3];
 var lessThan20 = numbers.filter((number) => number < 20);
 console.log(lessThan20);
+
+// 2
+var strings = ["winner", "winner", "chicken", "dinner"];
+var startsWithW = strings.filter((word) => word[0] === "w");
+console.log(startsWithW);
