832db1799005488db5591bbaf6a0b823333aeed9
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-21 16:20:06 -0500

    Added solution 2

--- map-practice.js
+++ map-practice.js
@@ -4,3 +4,11 @@
 var numbers = [1, 2, 3];
 var numbersTimes3 = numbers.map((number) => number * 3);
 console.log(numbersTimes3);
+
+// 2
+var people = [
+  { name: "Alice", age: 27 },
+  { name: "Blane", age: 16 },
+];
+var peopleNames = people.map((person) => person.name);
+console.log(peopleNames);
