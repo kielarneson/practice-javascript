61a6aa73436ddd3e784040853fc9c6e98be2d3db
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-22 17:12:06 -0500

    Added solution 3

--- select-practice.js
+++ select-practice.js
@@ -9,3 +9,12 @@ console.log(lessThan20);
 var strings = ["winner", "winner", "chicken", "dinner"];
 var startsWithW = strings.filter((word) => word[0] === "w");
 console.log(startsWithW);
+
+// 3
+var items = [
+  { name: "chair", price: 100 },
+  { name: "pencil", price: 1 },
+  { name: "book", price: 4 },
+];
+var priceGreaterThan5 = items.filter((item) => item.price > 5);
+console.log(priceGreaterThan5);
