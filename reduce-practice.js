d4ff9b066b06d5ec66590ce4566e131c502fad6e
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-23 17:01:09 -0500

    Added solution 3

--- reduce-practice.js
+++ reduce-practice.js
@@ -9,3 +9,12 @@ console.log(addNumbers);
 var strings = ["volleyball", "basketball", "badminton"];
 var addStrings = strings.reduce((finalString, word) => finalString + word);
 console.log(addStrings);
+
+// 3
+var items = [
+  { name: "chair", price: 100 },
+  { name: "pencil", price: 1 },
+  { name: "book", price: 4 },
+];
+var addPrice = items.map((item) => item.price).reduce((sum, current) => sum + current);
+console.log(addPrice);
