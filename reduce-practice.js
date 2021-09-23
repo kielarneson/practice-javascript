dd6bd07e77273f82c5ba5c0700ce8d222e1868f1
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-23 16:59:58 -0500

    Added solution 2

--- reduce-practice.js
+++ reduce-practice.js
@@ -4,3 +4,8 @@
 var numbers = [5, 10, 8, 3];
 var addNumbers = numbers.reduce((sum, number) => (sum += number));
 console.log(addNumbers);
+
+// 2
+var strings = ["volleyball", "basketball", "badminton"];
+var addStrings = strings.reduce((finalString, word) => finalString + word);
+console.log(addStrings);
